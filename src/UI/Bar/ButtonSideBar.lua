ec09b57222855508eab34e73c85a42d0aef4939c
TheNexusAvenger <zpcook@googlemail.com>
2020-02-04 00:00:45 -0500

    Reduced button size.

--- src/UI/Bar/ButtonSideBar.lua
+++ src/UI/Bar/ButtonSideBar.lua
@@ -25,16 +25,16 @@ function ButtonSideBar:__new()
 	--Create the buttons.
 	local RunTests = RunTestsButton.new()
 	RunTests.Hidden = true
-	RunTests.Size = UDim2.new(0,20,0,20)
-	RunTests.Position = UDim2.new(0,4,0,4)
+	RunTests.Size = UDim2.new(0,14,0,14)
+	RunTests.Position = UDim2.new(0,7,0,7)
 	RunTests.Parent = self
 	self:__SetChangedOverride("RunTestsButton",function() end)
 	self.RunTestsButton = RunTests
 	
 	local RunFailedTests = RunFailedTestsButton.new()
 	RunFailedTests.Hidden = true
-	RunFailedTests.Size = UDim2.new(0,20,0,20)
-	RunFailedTests.Position = UDim2.new(0,4,0,32)
+	RunFailedTests.Size = UDim2.new(0,14,0,14)
+	RunFailedTests.Position = UDim2.new(0,7,0,32)
 	RunFailedTests.Parent = self
 	self:__SetChangedOverride("RunFailedTestsButton",function() end)
 	self.RunFailedTestsButton = RunFailedTests
