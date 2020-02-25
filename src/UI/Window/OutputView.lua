7d82689c64005812c454fe7493df40de5b5375ff
TheNexusAvenger <zpcook@googlemail.com>
2020-02-24 22:39:47 -0500

    Implemented OutputView class.

---
+++ src/UI/Window/OutputView.lua
@@ -0,0 +1,234 @@
+--[[
+TheNexusAvenger
+
+Frame for viewing the output of a test.
+--]]
+
+local LINE_HEIGHT_PIXELS = 17
+local ENUMS_TO_COLORS = {
+	[Enum.MessageType.MessageOutput] = "MainText",
+	[Enum.MessageType.MessageWarning] = "WarningText",
+	[Enum.MessageType.MessageError] = "ErrorText",
+	[Enum.MessageType.MessageInfo] = "InfoText",
+}
+
+
+
+local NexusUnitTestingPluginProject = require(script.Parent.Parent.Parent)
+local NexusPluginFramework = NexusUnitTestingPluginProject:GetResource("NexusPluginFramework")
+
+local TextService = game:GetService("TextService")
+
+local OutputView = NexusUnitTestingPluginProject:GetResource("NexusPluginFramework.Base.NexusWrappedInstance"):Extend()
+OutputView:SetClassName("OutputView")
+
+
+
+--[[
+Creates a Output View frame object.
+--]]
+function OutputView:__new()
+	self:InitializeSuper("Frame")
+	
+	--Store the labels.
+	self:__SetChangedOverride("OutputLabels",function() end)
+	self.OutputLabels = {}
+	
+	--Store the output data.
+	self:__SetChangedOverride("OutputLines",function() end)
+	self.OutputLines = {}
+	self:__SetChangedOverride("MaxLineWidth",function() end)
+	self.MaxLineWidth = 0
+	self:__SetChangedOverride("TestEvents",function() end)
+	self.TestEvents = {}
+	
+	--Create the scrolling frame.
+	local ScrollingFrame = NexusPluginFramework.new("ScrollingFrame","Qt5")
+	ScrollingFrame.Hidden = true
+	ScrollingFrame.Size = UDim2.new(1,0,1,0)
+	ScrollingFrame.BackgroundTransparency = 1
+	ScrollingFrame.Parent = self
+	self:__SetChangedOverride("ScrollingFrame",function() end)
+	self.ScrollingFrame = ScrollingFrame
+	
+	--Create the output view.
+	local OutputClips = NexusPluginFramework.new("Frame")
+	OutputClips.Hidden = true
+	OutputClips.Size = UDim2.new(1,-17,1,0)
+	OutputClips.ClipsDescendants = true
+	OutputClips.Parent = self
+	self:__SetChangedOverride("OutputClips",function() end)
+	self.OutputClips = OutputClips
+	
+	local OutputContainer = NexusPluginFramework.new("Frame")
+	OutputContainer.Size = UDim2.new(1,1,1,0)
+	OutputContainer.Parent = OutputClips
+	self:__SetChangedOverride("OutputContainer",function() end)
+	self.OutputContainer = OutputContainer
+	
+	--Connect the events.
+	self:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+		self:UpdateTotalLabels()
+		self:UpdateContainerPoisiton()
+		self:UpdateDisplayedOutput()
+	end)
+	self:GetPropertyChangedSignal("CanvasPosition"):Connect(function()
+		self:UpdateContainerPoisiton()
+		self:UpdateDisplayedOutput()
+	end)
+	
+	--Set the defaults.
+	self.Size = UDim2.new(1,0,1,0)
+	self:UpdateTotalLabels()
+end
+
+--[[
+Returns if the scroll bar is at the bottom.
+Returns false if the scroll bar doesn't exist.
+--]]
+function OutputView:IsScrollBarAtBottom()
+	return self.ScrollingFrame.CanvasPosition.Y ~= 0 and self.ScrollingFrame.CanvasPosition.Y + self.ScrollingFrame.AbsoluteWindowSize.Y == #self.OutputLines * LINE_HEIGHT_PIXELS
+end
+
+--[[
+Adds and removes text labels depending on the
+size of the frame.
+--]]
+function OutputView:UpdateTotalLabels()
+	--Determine how many labels are needed.
+	local RequiredLabels = math.ceil(self.AbsoluteSize.Y/LINE_HEIGHT_PIXELS)
+	
+	--Add extra labels.
+	for i = 1,RequiredLabels do
+		if not self.OutputLabels[i] then
+			local NewLabel = NexusPluginFramework.new("TextLabel")
+			NewLabel.Size = UDim2.new(1,0,0,LINE_HEIGHT_PIXELS)
+			NewLabel.Position = UDim2.new(0,0,0,17 * (i - 1))
+			NewLabel.Text = ""
+			NewLabel.Parent = self.OutputContainer
+			self.OutputLabels[i] = NewLabel
+		end
+	end
+	
+	--Remove unneded labels.
+	for i = RequiredLabels + 1,#self.OutputContainer:GetChildren() do
+		self.OutputLabels[i]:Destroy()
+		self.OutputLabels[i] = nil
+	end
+end
+
+--[[
+Updates the size and position of the
+scrolling frame.
+--]]
+function OutputView:UpdateScrollBarSizes()
+	--Update the canvas size.
+	local IsAtBottom = self:IsScrollBarAtBottom()
+	self.ScrollingFrame.CanvasSize = UDim2.new(0,self.MaxLineWidth,0,#self.OutputLines * LINE_HEIGHT_PIXELS)
+	
+	--Update the clips size if a bottom scroll bar exists.
+	if self.MaxLineWidth > self.OutputClips.AbsoluteSize.X then
+		self.OutputClips.Size = UDim2.new(1,-17,1,-17)
+		if IsAtBottom then
+			self.ScrollingFrame.CanvasPosition = Vector2.new(self.ScrollingFrame.CanvasPosition.X,(#self.OutputLines * LINE_HEIGHT_PIXELS) - self.OutputClips.AbsoluteSize.Y + 16)
+		end
+	else
+		self.OutputClips.Size = UDim2.new(1,-17,1,0)
+		if IsAtBottom then
+			self.ScrollingFrame.CanvasPosition = Vector2.new(self.ScrollingFrame.CanvasPosition.X,(#self.OutputLines * LINE_HEIGHT_PIXELS) - self.OutputClips.AbsoluteSize.Y)
+		end
+	end
+end
+
+--[[
+Updates the position of the output
+container.
+--]]
+function OutputView:UpdateContainerPoisiton()
+	if self:IsScrollBarAtBottom() then
+		self.OutputContainer.Position = UDim2.new(0,-self.ScrollingFrame.CanvasPosition.X,0,-((math.ceil(self.ScrollingFrame.AbsoluteWindowSize.Y/LINE_HEIGHT_PIXELS) * LINE_HEIGHT_PIXELS) - self.ScrollingFrame.AbsoluteWindowSize.Y))
+	else
+		self.OutputContainer.Position = UDim2.new(0,-self.ScrollingFrame.CanvasPosition.X,0,0)
+	end
+end
+
+--[[
+Updates the displayed output.
+--]]
+function OutputView:UpdateDisplayedOutput()
+	local StartIndex = math.floor(self.ScrollingFrame.CanvasPosition.Y/LINE_HEIGHT_PIXELS)
+	
+	--Set the text to the output or unset the text.
+	for i = 1,math.ceil(self.ScrollingFrame.AbsoluteWindowSize.Y/LINE_HEIGHT_PIXELS) do
+		local EntryId = StartIndex + i
+		local OutputData = self.OutputLines[EntryId]
+		local OutputLabel = self.OutputLabels[i]
+		if OutputData then
+			OutputLabel.Text = OutputData[1]
+			OutputLabel.TextColor3 = ENUMS_TO_COLORS[OutputData[2]]
+		else
+			OutputLabel.Text = ""
+		end
+	end
+end
+
+--[[
+Adds a line to display in the output.
+--]]
+function OutputView:AddOutput(String,Type)
+	--If the string has multiple lines, split the string and add them.
+	if string.find(String,"\n") then
+		for _,SubString in pairs(string.split(String,"\n")) do
+			self:AddOutput(SubString,Type)
+		end
+		return
+	end
+	
+	--Add the string.
+	table.insert(self.OutputLines,{String,Type})
+	
+	--Update the max size.
+	local StringWidth = TextService:GetTextSize(String,14,"SourceSans",Vector2.new(2000,LINE_HEIGHT_PIXELS)).X + 4
+	if StringWidth > self.MaxLineWidth then
+		self.MaxLineWidth = StringWidth
+	end
+	
+	--Update the output view.
+	self:UpdateScrollBarSizes()
+	self:UpdateContainerPoisiton()
+	self:UpdateDisplayedOutput()
+end
+
+--[[
+Sets the test to use for the output.
+--]]
+function OutputView:SetTest(Test)
+	--Clear the output.
+	self.OutputLines = {}
+	self.MaxLineWidth = 0
+	
+	--Disconnect the existing events.
+	for _,Event in pairs(self.TestEvents) do
+		Event:Disconnect()
+	end
+	self.TestEvents = {}
+	
+	--Connect the events.
+	table.insert(self.TestEvents,Test.MessageOutputted:Connect(function(Message,Type)
+		self:AddOutput(Message,Type)
+	end))
+	
+	--Add the existing output.
+	for _,Output in pairs(Test.Output) do
+		self:AddOutput(Output[1],Output[2])
+	end
+	
+	--Update the displayed output if it wasn't done so already.
+	if #self.OutputLines == 0 then
+		self:UpdateDisplayedOutput()
+	end
+end
+
+
+
+return OutputView
---
+++ test/UI/Window/OutputViewTests.lua
@@ -0,0 +1,313 @@
+--[[
+TheNexusAvenger
+
+Tests the OutputView class.
+--]]
+
+local NexusUnitTesting = require("NexusUnitTesting")
+
+local NexusUnitTestingPlugin = require(game:GetService("ReplicatedStorage"):WaitForChild("NexusUnitTestingPlugin"))
+local OutputView = NexusUnitTestingPlugin:GetResource("UI.Window.OutputView")
+local UnitTestClass = NexusUnitTestingPlugin:GetResource("NexusUnitTestingModule.UnitTest.UnitTest")
+
+
+
+--[[
+Tests right amount of labels are made.
+--]]
+NexusUnitTesting:RegisterUnitTest("LabelEfficiency",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	UnitTest:AssertEquals(CuT.ClassName,"OutputView","Class name is incorrect.")
+	UnitTest:AssertEquals(#CuT:GetChildren(),0,"Child objects are exposed.")
+	
+	--Set the size and assert the correct amount of list frames exist.
+	CuT.Size = UDim2.new(0,200,0,170)
+	local Temp = CuT.AbsoluteSize
+	UnitTest:AssertEquals(#CuT.OutputContainer:GetChildren(),10,"Total labels is incorrect.")
+	
+	--Resize the frame and assert new labels were created.
+	CuT.Size = UDim2.new(0,200,0,200)
+	local Temp = CuT.AbsoluteSize
+	UnitTest:AssertEquals(#CuT.OutputContainer:GetChildren(),12,"Total labels is incorrect.")
+	
+	--Reisize the frame and assert labels were removed.
+	CuT.Size = UDim2.new(0,200,0,170)
+	local Temp = CuT.AbsoluteSize
+	UnitTest:AssertEquals(#CuT.OutputContainer:GetChildren(),10,"Total labels is incorrect.")
+end)
+
+--[[
+Tests the IsScrollBarAtBottom method.
+--]]
+NexusUnitTesting:RegisterUnitTest("IsScrollBarAtBottom",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,60)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Set the output below the size requirement and assert that the scroll bar isn't at the bottom.
+	CuT.OutputLines = {{"String 1"},{"String 2"}}
+	CuT.MaxLineWidth = 50
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertFalse(CuT:IsScrollBarAtBottom())
+	
+	--Set the text as requiring a bottom scroll bar and assert that the scroll bar isn't at the bottom.
+	CuT.MaxLineWidth = 250
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertFalse(CuT:IsScrollBarAtBottom())
+	
+	--Add additional output and assert that the scroll bar isn't at the bottom.
+	CuT.OutputLines = {{"String 1"},{"String 2"},{"String 3"},{"String 4"}}
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertFalse(CuT:IsScrollBarAtBottom())
+	
+	--Scroll the frame to the bottom and assert it is at the bottom.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,((4 * 17) + 16) - 60)
+	UnitTest:AssertTrue(CuT:IsScrollBarAtBottom())
+	
+	--Reduce the max length and assert the scroll bar position changed.
+	CuT.MaxLineWidth = 50
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertTrue(CuT:IsScrollBarAtBottom())
+	UnitTest:AssertEquals(CuT.ScrollingFrame.CanvasPosition.Y,(4 * 17) - 60,"Scroll bar wasn't changed.")
+	
+	--Increaase the max length and assert the scroll bar position changed.
+	CuT.MaxLineWidth = 250
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertTrue(CuT:IsScrollBarAtBottom())
+	UnitTest:AssertEquals(CuT.ScrollingFrame.CanvasPosition.Y,((4 * 17) + 16) - 60,"Scroll bar wasn't changed.")
+end)
+
+--[[
+Tests the UpdateScrollBarSizes method.
+--]]
+NexusUnitTesting:RegisterUnitTest("UpdateScrollBarSizes",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,50)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Set the output below the size requirement and assert the sizes are correct.
+	CuT.OutputLines = {{"String 1"},{"String 2"}}
+	CuT.MaxLineWidth = 50
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertEquals(CuT.ScrollingFrame.CanvasSize,UDim2.new(0,50,0,34),"Canvas size is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputClips.Size,UDim2.new(1,-17,1,0),"Clips size is incorrect.")
+	
+	--Set the max line width to requiring a bottom scroll bar and assert the sizes are correct.
+	CuT.MaxLineWidth = 250
+	CuT:UpdateScrollBarSizes()
+	UnitTest:AssertEquals(CuT.ScrollingFrame.CanvasSize,UDim2.new(0,250,0,34),"Canvas size is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputClips.Size,UDim2.new(1,-17,1,-17),"Clips size is incorrect.")
+end)
+
+--[[
+Tests the UpdateContainerPoisiton method.
+--]]
+NexusUnitTesting:RegisterUnitTest("UpdateContainerPoisiton",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,50)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Set the output below the height and assert the position is correct.
+	CuT.OutputLines = {{"String 1"},{"String 2"}}
+	CuT.MaxLineWidth = 50
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateContainerPoisiton()
+	UnitTest:AssertEquals(CuT.OutputContainer.Position,UDim2.new(0,0,0,0),"Position is incorrect.")
+	
+	--Set the output above the height and assert the position is correct.
+	CuT.OutputLines = {{"String 1"},{"String 2"},{"String 3"},{"String 4"},{"String 5"},{"String 6"}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateContainerPoisiton()
+	UnitTest:AssertEquals(CuT.OutputContainer.Position,UDim2.new(0,0,0,0),"Position is incorrect.")
+	
+	--Set the scroll bar position to the middle and assert the position is unchaged.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,10)
+	CuT:UpdateContainerPoisiton()
+	UnitTest:AssertEquals(CuT.OutputContainer.Position,UDim2.new(0,0,0,0),"Position is incorrect.")
+	
+	--Set the scroll bar position to the bottom and assert the position is unchaged.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,(6 * 17) - 50)
+	CuT:UpdateContainerPoisiton()
+	UnitTest:AssertEquals(CuT.OutputContainer.Position,UDim2.new(0,0,0,-1),"Position is incorrect.")
+end)
+
+--[[
+Tests the UpdateDisplayedOutput method.
+--]]
+NexusUnitTesting:RegisterUnitTest("UpdateDisplayedOutput",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,50)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Set the output as having no lines and assert the text is correct.
+	CuT.OutputLines = {}
+	CuT.MaxLineWidth = 50
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	
+	--Set the output as having not enough lines and assert the text is correct.
+	CuT.OutputLines = {{"String 1",Enum.MessageType.MessageOutput},{"String 2",Enum.MessageType.MessageWarning}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	
+	--Set the output as having more than enough lines and assert the text is correct.
+	CuT.OutputLines = {{"String 1",Enum.MessageType.MessageOutput},{"String 2",Enum.MessageType.MessageWarning},{"String 3",Enum.MessageType.MessageError},{"String 4",Enum.MessageType.MessageInfo},{"String 5",Enum.MessageType.MessageInfo}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 3","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"ErrorText","Text color is incorrect.")
+	
+	--Set the canvas position as nearly scrolled and assert that the correct lines show.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,10)
+	CuT.OutputLines = {{"String 1",Enum.MessageType.MessageOutput},{"String 2",Enum.MessageType.MessageWarning},{"String 3",Enum.MessageType.MessageError},{"String 4",Enum.MessageType.MessageInfo},{"String 5",Enum.MessageType.MessageInfo}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 3","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"ErrorText","Text color is incorrect.")
+	
+	--Set the canvas position as scrolled and assert that the correct lines show.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,20)
+	CuT.OutputLines = {{"String 1",Enum.MessageType.MessageOutput},{"String 2",Enum.MessageType.MessageWarning},{"String 3",Enum.MessageType.MessageError},{"String 4",Enum.MessageType.MessageInfo},{"String 5",Enum.MessageType.MessageInfo}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 3","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 4","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"WarningText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"ErrorText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"InfoText","Text color is incorrect.")
+	
+	--Set the canvas position to the bottom and assert that the correct lines show.
+	CuT.ScrollingFrame.CanvasPosition = Vector2.new(0,(17 * 5) - 50)
+	CuT.OutputLines = {{"String 1",Enum.MessageType.MessageOutput},{"String 2",Enum.MessageType.MessageWarning},{"String 3",Enum.MessageType.MessageError},{"String 4",Enum.MessageType.MessageInfo},{"String 5",Enum.MessageType.MessageInfo}}
+	CuT:UpdateScrollBarSizes()
+	CuT:UpdateDisplayedOutput()
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 3","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 4","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 5","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"ErrorText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"InfoText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"InfoText","Text color is incorrect.")
+end)
+
+--[[
+Tests the AddOutput method.
+--]]
+NexusUnitTesting:RegisterUnitTest("AddOutput",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,50)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Add an empty string and assert the output is correct.
+	CuT:AddOutput("",Enum.MessageType.MessageOutput)
+	UnitTest:AssertEquals(CuT.OutputLines,{{"",Enum.MessageType.MessageOutput}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	
+	--Add a string and assert the output is correct.
+	CuT:AddOutput("String 1",Enum.MessageType.MessageWarning)
+	UnitTest:AssertEquals(CuT.OutputLines,{{"",Enum.MessageType.MessageOutput},{"String 1",Enum.MessageType.MessageWarning}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	
+	--Add a string with a new line and assert the output is correct.
+	CuT:AddOutput("String 2\nString 3\n",Enum.MessageType.MessageOutput)
+	UnitTest:AssertEquals(CuT.OutputLines,{{"",Enum.MessageType.MessageOutput},{"String 1",Enum.MessageType.MessageWarning},{"String 2",Enum.MessageType.MessageOutput},{"String 3",Enum.MessageType.MessageOutput},{"",Enum.MessageType.MessageOutput}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"MainText","Text color is incorrect.")
+end)
+
+--[[
+Tests the SetTest method.
+--]]
+NexusUnitTesting:RegisterUnitTest("SetTest",function(UnitTest)
+	--Create the component under testing.
+	local CuT = OutputView.new()
+	CuT.Size = UDim2.new(0,200,0,50)
+	CuT.Parent = Instance.new("ScreenGui",game:GetService("Lighting"))
+	delay(0.1,function() CuT.Parent:Destroy() end)
+	
+	--Create 3 tests.
+	local Test1,Test2,Test3 = UnitTestClass.new("Test 1"),UnitTestClass.new("Test 2"),UnitTestClass.new("Test 3")
+	Test2:OutputMessage(Enum.MessageType.MessageOutput,"String 4")
+	Test2:OutputMessage(Enum.MessageType.MessageWarning,"String 5")
+	
+	--Set a test, add messages, and assert the messages are displayed.
+	CuT:SetTest(Test1)
+	Test1:OutputMessage(Enum.MessageType.MessageError,"String 1")
+	Test1:OutputMessage(Enum.MessageType.MessageInfo,"String 2\nString 3")
+	UnitTest:AssertEquals(CuT.OutputLines,{{"String 1",Enum.MessageType.MessageError},{"String 2",Enum.MessageType.MessageInfo},{"String 3",Enum.MessageType.MessageInfo}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 1","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 2","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"String 3","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"ErrorText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"InfoText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].TextColor3,"InfoText","Text color is incorrect.")
+	
+	--Set a test with an existting output and assert the messages are displayed.
+	CuT:SetTest(Test2)
+	UnitTest:AssertEquals(CuT.OutputLines,{{"String 4",Enum.MessageType.MessageOutput},{"String 5",Enum.MessageType.MessageWarning}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 4","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 5","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	
+	--Send an output for a previous test and assert it wasn't added.
+	Test1:OutputMessage(Enum.MessageType.MessageError,"Fail")
+	UnitTest:AssertEquals(CuT.OutputLines,{{"String 4",Enum.MessageType.MessageOutput},{"String 5",Enum.MessageType.MessageWarning}},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"String 4","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"String 5","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].TextColor3,"MainText","Text color is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].TextColor3,"WarningText","Text color is incorrect.")
+	
+	--Set a test with no existing output and assert the messages are displayed.
+	CuT:SetTest(Test3)
+	UnitTest:AssertEquals(CuT.OutputLines,{},"Stored output is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[1].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[2].Text,"","Text is incorrect.")
+	UnitTest:AssertEquals(CuT.OutputLabels[3].Text,"","Text is incorrect.")
+end)
+
+
+
+return true
