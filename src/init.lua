5e28cee62b33e07d342e622d4e38f8204e7260da
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-02-26 02:57:53 -0500

    Remove Nexus Plugin Framework.

--- src/init.lua
+++ src/init.lua
@@ -5,8 +5,8 @@ Project for the Nexus Unit Testing Plugin.
 --]]
 
 local NexusProject = require(script:WaitForChild("NexusProject"))
-local NexusPluginFrameworkPluginProject = NexusProject.new(script)
+local NexusUnitTestingPluginProject = NexusProject.new(script)
 
 
 
-return NexusPluginFrameworkPluginProject
+return NexusUnitTestingPluginProject
